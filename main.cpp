e5ea9673cc14de7fa1a978120ad3872683fd9d67
Daniel Perez-Rodriguez <112608773+Wemisiu@users.noreply.github.com>
2024-05-01 01:29:16 +0000

    Working things out

--- main.cpp
+++ main.cpp
@@ -1,15 +1,65 @@
+#include <iostream>
+using namespace std;
 
 
-void findMaxR(TreeNode * current) {
+// Define a new class, BinaryTree
 
-    // go all the way to the right
+class TreeNode {
+    public:
+        int data;
+        TreeNode *right;
+        TreeNode *left;
 
-    if (current -> right == nullptr) {
+        // define a constructor for this node
+            // if no input data parameter, value can be zero
+        TreeNode(int d = 0) {
+            data = d;
+        }
+};
 
-        return cout << current-> data;
-    }
+class BinaryTree {
+    public:
+        TreeNode *root;
 
-    findMaxR(current -> right);
+    // define a function to print all values in the tree
+        // if the tree is empty, return
+        
+        // if not, print the node
+            //then, print the subtree on the right
+            // then, print the subtree on the left
 
-   
+
+};
+
+
+
+int main() {
+    // Declare a Binary Tree Object
+    BinaryTree myTree;
+
+    // access the root attribute, and place in a treeNode with 10
+    myTree.root = new TreeNode(100);
+
+    // create the children of the root node
+        // 6 on the left and 5 on the right
+    myTree.root -> left = new TreeNode(10);
+    myTree.root -> right = new TreeNode(200);
+
+    // create the level 2 nodes
+        // 2 is left child of 6
+        // 20 is the left child of 5
+        // 100 is the right child of 5
+    myTree.root -> left -> left = new TreeNode(5);
+    myTree.root -> right -> left = new TreeNode(202);
+
+    // create the level 3 nodes
+    myTree.root -> left -> left -> right = new TreeNode(100);
+
+    cout << "Hello World!";
+
+
 }
+
+
+
+
