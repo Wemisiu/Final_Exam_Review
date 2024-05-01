094e6a18d17c6a8b3337df7d2a87c9fb97b71834
Daniel Perez-Rodriguez <112608773+Wemisiu@users.noreply.github.com>
2024-05-01 20:35:47 +0000

    Trying to get things back

--- main.cpp
+++ main.cpp
@@ -7,37 +7,8 @@ using namespace std;
 
 int main() {
    
- double arr [] = {9.2,0.3,-6.6,10.1,0.02};
 
- double  * ptr; 
-
- ptr = arr + 2;
-
-cout << *ptr;
-
- ptr =ptr - 1;
-
-cout << *ptr;
-
-double * ptr2;
-
-ptr = arr + 4;
-
-cout << *ptr + *ptr2;
-
-
-
-for(int i = 0; i < 5; i++) {
-
-if(ptr == i) {
-
-    // skip
-}
-
-cout << arr[i] << " ";
-
-}
-
+rightmost(root);
 
 
 
