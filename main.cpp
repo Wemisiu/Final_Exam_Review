0361496df3a4913e3aecf88a53da27422a602b0b
Daniel Perez-Rodriguez <112608773+Wemisiu@users.noreply.github.com>
2024-05-01 19:09:30 +0000

    creating pointer list

--- main.cpp
+++ main.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 #include "binary.h"
 
+
 int main() {
-    TreeNode * root = buildTree();
+   
+double arr [] = {9.2,0.3,-6.6,10.1,0.02};
 
-    print(root);
+double  * ptr; 
 
-    
+ptr = arr + 3;
+
+cout << *ptr;
+
+
   
 }
